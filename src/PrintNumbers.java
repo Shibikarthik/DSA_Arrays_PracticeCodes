38c8e19ef5254dea16d4af40dfc979cf135f3e87
Shibikarthik <shibirafa777@gmail.com>
2025-02-25 18:34:59 +0530

    First commit

---
+++ src/PrintNumbers.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PrintNumbers {
+    public static void main(String[] args) {
+
+        int n =10;
+
+        for(int i=0; i<n;i++){
+
+            System.out.println(i);
+        }
+    }
+}
